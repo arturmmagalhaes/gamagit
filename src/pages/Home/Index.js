082c0ee282b3b4bf0e49c7af77e7f67154ca9b6e
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-05-20 22:28:22 -0300

    Salvando o nome dos repositórios no localStorage

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -6,11 +6,17 @@ import * as S from './styled';
 function Home() {
   const [ usuario, setUsuario ] = useState("");
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(Response => console.log(Response));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(Response => {
+      const repositories = Response.data;
+      const repositoriesName = []
+      repositories.map((repository) =>{
+        repositoriesName.push(repository.name)
+      });
+      localStorage.setItem("repositories", JSON.stringify(repositoriesName))
+    });
   }
   return (
     <S.Container>
-      <p>{usuario}</p>
       <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
       <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>  
     </S.Container>
